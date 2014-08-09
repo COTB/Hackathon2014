da426e5a2ff0799ee7e7f1fa9d8537dfc26d970f
James Blair <james.blair@outlook.com>
2014-08-09 15:52:16 -0400

    added missing file

---
+++ K9s.WarriorPortal/K9s.WarriorPortal.Services/WarriorApplicationQueryService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using K9s.WarriorPortal.Models;
+using K9s.WarriorPortal.Models.Enum;
+using K9s.WarriorPortal.Services.Contracts;
+
+namespace K9s.WarriorPortal.Services
+{
+    public class WarriorApplicationQueryService : IWarriorApplicationQueryService
+    {
+        private static readonly IEnumerable<ApplicationStatus> OpenStatuses =
+            new[] {ApplicationStatus.Applied, ApplicationStatus.Processing };
+
+        private readonly IDbContextFactory<WarriorPortalDbContext> _dbContextFactory;
+
+        public WarriorApplicationQueryService(IDbContextFactory<WarriorPortalDbContext> dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        public async Task<IList<WarriorApplication>> GetOpenApplicationsAsync()
+        {
+            using (var context = _dbContextFactory.Create())
+            {
+                return await context.WarriorApplications
+                    .Where(application => OpenStatuses.Contains(application.ApplicationStatus))
+                    .ToListAsync();
+            }
+        }
+    }
+}
